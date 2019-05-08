be060e9ead537e70d9bd0b04e92612ef34fda630
kringdong <kringdong@gmail.com>
2019-05-09 02:37:31 +0930

    HTML: edited nested list, added a header to check that sticky nav doesn't cover the header, added content to test sticky nav with scrolling
    CSS: added grid, arrow and most styling for the nav, also made nav sticky
    
    JS: added open and close functions for mobile nav

---
+++ templates/scripts/nav.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("mobile-nav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mobile-nav").style.display = "none";
+}
